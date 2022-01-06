3cc36e76ba7a25c9dc64d3002dea11904209b51b
Jossyelmos <jossyelmos@yahoo.com>
2022-01-06 22:53:32 +0200

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import image from '../images/amos-2.jpg';
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <div className="all-center py-3">
+                <img 
+                    src={image} 
+                    alt="img" 
+                    className='round-img'
+                    style={{ width: '200px', height: '200px' }} 
+                />
+                <div className='homeInfo text-center'>
+                    <div className='infoCls my-3'>
+                        <h1>i'm amos oluniyi. <span>web developer</span></h1>
+                        <p>I'm a South African based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
+                        <p>Building simple and beautiful things for complex interfaces is what I enjoy most about my work. I am also interested in crafting beautiful minimal products and exploring new worlds.</p>
+                        <p>Contact: <strong>jossyelmos@yahoo.com</strong></p>
+                        <p className="social-img">
+                            <a href='#!'><i className="fab fa-react fa-3x"></i> </a>
+                            <a href='#!'><i className="fab fa-html5 fa-3x"></i> </a>
+                            <a href='#!'><i className="fab fa-css3-alt fa-3x"></i> </a>
+                            <a href='#!'><i className="fab fa-node fa-3x"></i> </a>
+                            <a href='#!'><i className="fab fa-js-square fa-3x"></i> </a>
+                            <a href='#!'><img src="https://img.Icons8.com/color/48/000000/firebase.png" alt='' /></a>
+                        </p>
+                        <a href='/portfolio' className='botton pink'>MORE ABOUT ME<i className='fa fa-arrow-right homeIcon active text-light' /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/Portfolio.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Portfolio = () => {
+    const portfolios = [
+        {
+            'id': '1',
+            'imageURL': 'https://spliteq.co.za/wp-content/uploads/2020/07/contact-2.jpg',
+            'link': 'https://lit-island-75764.herokuapp.com/',
+            'html': 'MANAGE-IT'
+        },
+        {
+            'id': '2',
+            'imageURL': 'https://images.ctfassets.net/lzny33ho1g45/how-to-push-to-github-p-img/2cdf1242bb3c892e4c8814dd123ca321/file.png?w=1520&fm=jpg&q=30&fit=thumb&h=760',
+            'link': 'https://github-finder-nine-eta.vercel.app/',
+            'html': 'GITHUB FINDER'
+        },
+        {
+            'id': '3',
+            'imageURL': 'https://www.thebalancesmb.com/thmb/kectf9d4azgI8yVnBuoB0h2Z8zA=/3865x2174/smart/filters:no_upscale()/overhead-view-of-smiling-female-friends-sharing-lunch-in-restaurant-928010348-5b4abe8f46e0fb003712c478.jpg',
+            'link': 'https://mazaa.vercel.app/',
+            'html': 'MAZAA RECIPES'
+        },
+    ]
+
+    const [isHovering, setIsHovering] = useState(-1);
+
+    return (
+        <div className="container">
+            <div className='port all-center py-3'>
+                <h1>my <span>project</span></h1>
+                <div className='grid-3'>
+                    {portfolios.map((item) => {
+                        return (
+                            <div className="" key={item.id}>    
+                                <a href={item.link} onMouseEnter={() => setIsHovering(item.id)} onMouseLeave={() => setIsHovering(-1)}>
+                                    <h3 className={isHovering === item.id ? 'port_html' : 'hidden'}>{item.html}</h3>
+                                    <img 
+                                        src={item.imageURL} 
+                                        alt= ''
+                                        style={{ height: '200px', borderRadius: '5px' }}
+                                        className={isHovering === item.id ? 'hidden' : ''}
+                                    />
+                                </a>   
+                            </div>
+                        )
+                    })}                 
+                </div>
+                <Link to='/' className='btn btn-dark my-3'>
+                    Back to Profile
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Portfolio;
